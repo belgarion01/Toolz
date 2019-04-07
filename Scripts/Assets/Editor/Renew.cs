d7dad1d76e182c4b058116da85b334af6771df5f
Unknown <hugo.raguin@gmail.com>
2019-04-07 19:08:47 +0200

    Rework du premier tool en cours

--- Scripts/Assets/Editor/Renew.cs
+++ Scripts/Assets/Editor/Renew.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System.Linq;
+using System;
 
 public class Renew : EditorWindow
 {
@@ -13,17 +15,162 @@ public class Renew : EditorWindow
     }
 
     private string spriteFolderPath;
-    private string spriteFilesPath;
+    private string[] spriteFilesPath;
+
     private string normalFolderPath;
+    private string[] normalFilesPath;
+
+    private Texture2D[] sprites;
+    private Texture2D[] normals;
+
+
+    bool createNewFolder = false;
+
+    int numberOfLoadedFiles = 0;
+    int numberOfUnloadedFiles = 0;
+
+    List<PreMaterialo> preMaterials = new List<PreMaterialo>();
+
+    string filesDirectory;
+
+    bool loadFinished = false;
 
     private void OnGUI()
     {
+        string projectMaterialsFolderPath = AssetDatabase.IsValidFolder("Assets/Materials") ? "Assets/Materials" : "";
+        string projectSpritesFolderPath = AssetDatabase.IsValidFolder("Assets/Sprites") ? "Assets/Sprites" : "";
+        string projectNormalsFolderPath = AssetDatabase.IsValidFolder("Assets/Textures") ? "Assets/Textures" : "";
+
         #region Selection des dossiers
         if (spriteFolderPath != null) GUILayout.Label(Path.GetFileName(spriteFolderPath), EditorStyles.helpBox);
-        spriteFolderPath = EditorUtility.OpenFolderPanel("Choisir le dossier où se trouve les sprites", "", "");
+        if (GUILayout.Button("Importer sprites")){
+            spriteFolderPath = EditorUtility.OpenFolderPanel("Choisir le dossier où se trouve les sprites", projectSpritesFolderPath, "");
+        }
+
         if (normalFolderPath != null) GUILayout.Label(Path.GetFileName(normalFolderPath), EditorStyles.helpBox);
-        normalFolderPath = EditorUtility.OpenFolderPanel("Choisir le dossier où se trouve les normals", "", "");
+        if (GUILayout.Button("Importer normals"))
+        {
+            normalFolderPath = EditorUtility.OpenFolderPanel("Choisir le dossier où se trouve les normals", projectNormalsFolderPath, "");
+        }
+        if ((spriteFolderPath == null || spriteFolderPath == "") || (normalFolderPath == null || normalFolderPath == "")) return;
         #endregion
 
+        if (GUILayout.Button("Charger les fichier"))
+        {
+            spriteFilesPath = Directory.GetFiles(spriteFolderPath, "*.png");
+            string[] spriteRelativeFilesPath = new string[spriteFilesPath.Length];
+            for (int i = 0; i < spriteFilesPath.Length; i++) {
+                string relativePath = "Assets"+spriteFilesPath[i].Substring(Application.dataPath.Length);
+                spriteRelativeFilesPath[i] = relativePath;
+            }
+
+            normalFilesPath = Directory.GetFiles(normalFolderPath, "*.png");
+            string[] normalRelativeFilesPath = new string[normalFilesPath.Length];
+            for (int i = 0; i < normalFilesPath.Length; i++)
+            {
+                string relativePath = "Assets" + normalFilesPath[i].Substring(Application.dataPath.Length);
+                normalRelativeFilesPath[i] = relativePath;
+            }
+
+
+            for (int i = 0; i < spriteFilesPath.Length; i++)
+            {
+                string crunchedFileName = Path.GetFileNameWithoutExtension(spriteFilesPath[i]);
+                crunchedFileName = crunchedFileName.Substring(4, Path.GetFileNameWithoutExtension(spriteFilesPath[i]).Length - 4);
+                PreMaterialo premat = CreatePreMaterial(crunchedFileName);
+                premat.sprite = (Texture2D)AssetDatabase.LoadAssetAtPath(spriteRelativeFilesPath[i], typeof(Texture2D));
+                preMaterials.Add(premat);
+            }
+            for (int i = 0;i<normalFilesPath.Length;i++) {
+                string crunchedFileName = Path.GetFileNameWithoutExtension(normalFilesPath[i]);
+                crunchedFileName = crunchedFileName.Substring(7, Path.GetFileNameWithoutExtension(normalFilesPath[i]).Length - 7);
+                
+                bool gotALover = false;
+                foreach (PreMaterialo premat in preMaterials) {
+                    if (premat.name == crunchedFileName)
+                    {
+                        premat.normal = (Texture2D)AssetDatabase.LoadAssetAtPath(normalRelativeFilesPath[i], typeof(Texture2D));
+                        gotALover = true;
+                    }
+                }
+                if (!gotALover)
+                {
+                    numberOfUnloadedFiles++;
+                }
+            }
+            loadFinished = true;
+        }
+        if (!loadFinished) return;
+        GUILayout.Label("Création de " + preMaterials.Count + " fichiers");
+        GUILayout.Label("Nombre de fichiers échoués : " + numberOfUnloadedFiles);
+        if (GUILayout.Button("Créer les matériaux")) {
+            filesDirectory = EditorUtility.OpenFolderPanel("Choisir le dossier où placer les matériaux", projectMaterialsFolderPath, "");
+            
+        }
+        if (filesDirectory != null && filesDirectory != "")
+        {
+            string filesDirectoryRelativePath = "Assets" + filesDirectory.Substring(Application.dataPath.Length);
+            foreach (PreMaterialo premat in preMaterials)
+            {
+                Debug.Log(premat.normal);
+                if (premat.name != null && premat.sprite != null && premat.normal != null)
+                {
+                    Debug.Log("C'est passé");
+                    AssetDatabase.CreateAsset(CreateNormalMaterial(premat), filesDirectoryRelativePath + "/" + premat.name + "_mat.mat");                  
+                }
+            }
+            AssetDatabase.Refresh();
+            Reset();
+        }
     }
+
+    private void Reset()
+    {
+        spriteFolderPath = null;
+        normalFolderPath = null;
+        spriteFilesPath = null;
+        normalFilesPath = null;
+        filesDirectory = null;
+        numberOfLoadedFiles = 0;
+        numberOfUnloadedFiles = 0;
+        loadFinished = false;
+        preMaterials.Clear();
+    }
+
+    private Material CreateNormalMaterial(PreMaterialo premat)
+    {
+        Material mat = new Material(Shader.Find("Legacy Shaders/Transparent/Bumped Diffuse"));
+        mat.SetTexture("_MainTex", premat.sprite);
+        mat.SetTexture("_BumpMap", premat.normal);
+        return mat;
+    }
+
+    private PreMaterialo CreatePreMaterial(string name)
+    {
+        PreMaterialo mat = new PreMaterialo();
+        mat.name = name;
+        return mat;
+    }
+
+    private PreMaterialo GetPreMaterial(string assetName)
+    {
+        if (preMaterials.Count > 0)
+        {
+            foreach (PreMaterialo premat in preMaterials)
+            {
+                if (premat.name == assetName)
+                {
+                    return premat;
+                }
+            }
+        }
+        return null;
+    }
+}
+
+public class PreMaterialo
+{
+    public string name;
+    public Texture2D sprite;
+    public Texture2D normal;
 }
--- Scripts/Assets/Editor/SpriteNormalImporter1.cs
+++ Scripts/Assets/Editor/SpriteNormalImporter1.cs
@@ -6,7 +6,7 @@ using UnityEditor;
 
 namespace Majoras
 {
-    /*public class SpriteNormalImporter : EditorWindow
+    public class SpriteNormalImporter : EditorWindow
     {
         [MenuItem("Window/Sprite + Normal Importer")]
         public static void ShowWindow()
@@ -14,37 +14,104 @@ namespace Majoras
             SpriteNormalImporter window = GetWindow<SpriteNormalImporter>("Sprite Importer");
         }
 
-        string folderPath;
-        string[] filesPath;
+        //string folderPath;
+        //string[] filesPath;
         List<PreMaterial> preMaterials = new List<PreMaterial>();
         bool customePixelsPerUnit = true;
         int pixelsPerUnit;
         bool isPixelArt = true;
+        List<File> files;
+        Folder materialsDirectoryFolder;
+        Folder spriteOriginFolder;
+        Folder normalOriginFolder;
+        Folder spriteDirectoryFolder;
+        Folder normalDirectoryFolder;
 
         private void OnGUI()
         {
 
-            #region Selection du dossier
-            if (folderPath != null) GUILayout.Label(Path.GetFileName(folderPath), EditorStyles.helpBox);
-
-            if (GUILayout.Button("Selectionner le dossier"))
+            #region Selection des dossiers (Origine et Destination)
+            #region Dossiers sprite
+            if (spriteOriginFolder != null) GUILayout.Label(spriteOriginFolder.name, EditorStyles.helpBox);
+            
+            if (GUILayout.Button("Origine des sprites"))
             {
-                folderPath = EditorUtility.OpenFolderPanel("Choisir le dossier à importer", "", "");
+                string folderPath = EditorUtility.OpenFolderPanel("Selectionner le dossier où se trouve les sprites", "", ""); 
+                if (folderPath != null && folderPath != "")
+                {
+                    spriteOriginFolder = new Folder(folderPath);
+                }
             }
 
-            if (folderPath == null) return;
+            if (GUILayout.Button("Destination des sprites")) {
+                string projectSpritesFolderPath = AssetDatabase.IsValidFolder("Assets/Sprites") ? "Assets/Sprites" : "";
+                string folderPath = EditorUtility.OpenFolderPanel("Selectionner le dossier où importer les sprites", projectSpritesFolderPath, "");
+                spriteDirectoryFolder = new Folder(folderPath);
+            }
             #endregion
-            isPixelArt = EditorGUILayout.Toggle("Assets en Pixel Art", isPixelArt);
+            #region Dossier normals
+            if (GUILayout.Button("Dossier des normals"))
+            {
+                string folderPath = EditorUtility.OpenFolderPanel("Selectionner le dossier où se trouve les normals", "", ""); 
+                if (folderPath != null && folderPath != "")
+                {
+                    normalOriginFolder = new Folder(folderPath);
+                }
+            }
+
+            if (GUILayout.Button("Destination des normals"))
+            {
+                string projectNormalsFolderPath = AssetDatabase.IsValidFolder("Assets/Textures") ? "Assets/Textures" : "";
+                string folderPath = EditorUtility.OpenFolderPanel("Selectionner le dossier où importer les normals", projectNormalsFolderPath, "");
+                normalDirectoryFolder = new Folder(folderPath);
+            }
+            #endregion
+            #endregion
+
+            if (spriteOriginFolder == null || normalOriginFolder == null || spriteDirectoryFolder == null || normalDirectoryFolder == null) return;
+
+            #region Paramètres supplémentaires
+            isPixelArt = EditorGUILayout.Toggle("Assets en Pixel Art", isPixelArt); //Ptetre
             customePixelsPerUnit = EditorGUILayout.Toggle("Valeur de Pixel Per Unit", customePixelsPerUnit);
             if (customePixelsPerUnit)
             {
                 pixelsPerUnit = EditorGUILayout.IntField("Valeur de PixelPerUnit", pixelsPerUnit);
             }
+            #endregion
+
 
             if (GUILayout.Button("Importer"))
             {
-                string folderName = Path.GetFileName(folderPath);
-                filesPath = Directory.GetFiles(folderPath);
+                string projectMaterialsFolderPath = AssetDatabase.IsValidFolder("Assets/Materials") ? "Assets/Materials" : "";
+                string folderPath = EditorUtility.OpenFolderPanel("Selectionner le dossier où vous voulez créer vos matériaux", projectMaterialsFolderPath, "");
+                materialsDirectoryFolder = new Folder(folderPath);
+
+                string folderName = Path.GetFileName(folderPath);//A ENLEVER
+
+                string[] filesPath = Directory.GetFiles(folderPath);//
+                foreach (string filePath in filesPath) {
+                    files.Add(new File(filePath));
+                }
+
+                foreach (File file in files) {
+                    //Créer le dossier Sprite
+                    //Créer un dossier dans Sprites au nom du fichier
+                    //C'est créer par Marie et storer dans spriteDirectoryFolder
+                    //Copier les fichiers dans ce dossier
+                    FileUtil.CopyFileOrDirectory(file.fullPath, spriteDirectoryFolder.relativePath + "/" + file.fullName);
+                    //Vérifie si un preMaterial porte son nom
+                        //Si oui, va dedans en tant que sprite
+                        //Si non, créer un preMaterial portant son nom et son sprite
+                    //Change les paramètres d'importation
+
+                    //Meme chose pour les normals
+
+                    //Créer le dossier Materials
+                    //Vérifie si les preMaterials sont bien remplies
+                        //Si oui, les créer dans le fichier portant le nom du dossier
+                    
+                    //Reset
+                }
 
                 foreach (string filePath in filesPath)
                 {
@@ -59,8 +126,8 @@ namespace Majoras
                         AssetDatabase.Refresh();
 
                         //Copier / Coller le sprite dans Sprites/ LeNomDuDossier
-                        if (!AssetDatabase.IsValidFolder("Assets/Sprites/" + folderName)) AssetDatabase.CreateFolder("Assets/Sprites", folderName);
-                        FileUtil.CopyFileOrDirectory(filePath, "Assets/Sprites/" + Path.GetFileName(folderPath) + "/" + Path.GetFileName(filePath));
+                        if (!AssetDatabase.IsValidFolder("Assets/Sprites/" + folderName)) AssetDatabase.CreateFolder("Assets/Sprites", folderName); //A ENLEVER
+                        FileUtil.CopyFileOrDirectory(filePath, "Assets/Sprites/" + Path.GetFileName(folderPath) + "/" + Path.GetFileName(filePath)); //
                         AssetDatabase.Refresh();
                         //Vérifie si un prematerial a déjà ce nom. Si oui, donne le sprite. Si non, créer un matérial avec le nom et le sprite puis l'ajoute à la liste.
                         if (GetPreMaterial(crunchedFileName) != null)
@@ -93,7 +160,7 @@ namespace Majoras
                         AssetDatabase.Refresh();
 
                         //Copier / Coller le sprite dans Sprites/ SonNom
-                        if (!AssetDatabase.IsValidFolder("Assets/Textures/" + folderName)) AssetDatabase.CreateFolder("Assets/Textures", folderName);
+                        if (!AssetDatabase.IsValidFolder("Assets/Textures/" + folderName)) AssetDatabase.CreateFolder("Assets/Textures", folderName); //A ENLEVER
                         FileUtil.CopyFileOrDirectory(filePath, "Assets/Textures/" + Path.GetFileName(folderPath) + "/" + Path.GetFileName(filePath));
                         AssetDatabase.Refresh();
                         //Vérifie si un prematerial a déjà ce nom. Si oui, donne le sprite. Si non, créer un matérial avec le nom et le sprite puis l'ajoute à la liste.
@@ -130,9 +197,11 @@ namespace Majoras
 
         private void Reset()
         {
+            /*
             folderPath = null;
             filesPath = null;
             preMaterials.Clear();
+            */
         }
 
         private Material CreateNormalMaterial(PreMaterial premat)
@@ -171,5 +240,43 @@ namespace Majoras
         public string name;
         public Texture2D sprite;
         public Texture2D normal;
-    }*/
+    }
+
+    public class File {
+        public string fullName;
+        public string nameWithoutExtension;
+        public string crunchedName;
+        public string fullPath;
+        public string relativePath;
+
+        public enum TypesOfFile { Null, Sprite, Normal };
+        public TypesOfFile fileType;
+
+        public File(string path) {
+            fullPath = path;
+            fullName = Path.GetFileName(path);
+            nameWithoutExtension = Path.GetFileNameWithoutExtension(path);
+            if (nameWithoutExtension.StartsWith("spr_")){
+                fileType = TypesOfFile.Sprite;
+                crunchedName = nameWithoutExtension.Substring(4, nameWithoutExtension.Length - 4);
+            }
+            else if (nameWithoutExtension.StartsWith("normal_")){
+                fileType = TypesOfFile.Normal;
+                crunchedName = nameWithoutExtension.Substring(7, nameWithoutExtension.Length - 7);
+            }
+            relativePath = "Assets" + fullPath.Substring(Application.dataPath.Length);
+        }
+    }
+
+    public class Folder {
+        public string name;
+        public string fullPath;
+        public string relativePath;
+
+        public Folder(string path) {
+            fullPath = path;
+            relativePath = "Asset"+fullPath.Substring(Application.dataPath.Length);
+            name = Path.GetFileName(path);
+        }
+    }
 }
